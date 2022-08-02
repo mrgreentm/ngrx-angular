d55ed4cb4936c1a8ca5c80e56c85a20df6962331
Joao Souza <joaonetotfm@gmail.com>
2022-08-01 21:23:43 -0300

    feature: implements reduces

---
+++ src/app/store/users/users.reducer.ts
@@ -0,0 +1,79 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { UserInterface } from './../../models/users.interface';
+import * as fromUsersActions from '../users/users.actions';
+
+export interface UsersState {
+  users: UserInterface[];
+  user: UserInterface | null;
+  error: string;
+}
+
+export const initialUsersState: UsersState = {
+  users: [],
+  user: null,
+  error: '',
+};
+
+const _usersReduce = createReducer(
+  initialUsersState,
+  //LOAD USERS
+  on(fromUsersActions.LoadUsersSuccess, (state, { payload }) => ({
+    ...state,
+    usuarios: payload,
+    error: '',
+  })),
+  on(fromUsersActions.LoadUserFail, (state, { error }) => ({
+    ...state,
+    error: error,
+  })),
+
+  //LOAD USER
+  on(fromUsersActions.LoadUser, (state, { payload }) => ({
+    ...state,
+    usuarios: payload,
+    error: '',
+  })),
+  on(fromUsersActions.LoadUserFail, (state, { error }) => ({
+    ...state,
+    error: error,
+  })),
+
+  //CREATE USER
+  on(fromUsersActions.CreateUser, (state, { payload }) => ({
+    ...state,
+    usuarios: [...state.users, payload],
+    error: '',
+  })),
+  on(fromUsersActions.CreateUserFail, (state, { error }) => ({
+    ...state,
+    error: error,
+  })),
+
+  //UPDATE USER
+  on(fromUsersActions.UpdateUser, (state, { payload }) => ({
+    ...state,
+    usuarios: [...state.users].map((row) => {
+      return row.id === payload.id ? payload : row;
+    }),
+    error: '',
+  })),
+  on(fromUsersActions.UpdateUserFail, (state, { error }) => ({
+    ...state,
+    error: error,
+  })),
+
+  //DELETE USER
+  on(fromUsersActions.DeleteUserSuccess, (state, { payload }) => ({
+    ...state,
+    usuarios: [...state.users].filter((user) => user.id !== payload),
+    error: '',
+  })),
+  on(fromUsersActions.DeleteUserFail, (state, { error }) => ({
+    ...state,
+    error: error,
+  }))
+);
+
+export function usersReduce(state = initialUsersState, action: Action) {
+  return _usersReduce(state, action);
+}
