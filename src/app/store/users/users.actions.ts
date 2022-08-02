5b7b58d5e7e85ff8827643ce00410179c443426b
Joao Souza <joaonetotfm@gmail.com>
2022-08-01 21:01:16 -0300

    feature: implements action

--- src/app/store/users/users.actions.ts
+++ src/app/store/users/users.actions.ts
@@ -1,3 +1,6 @@
+import { UserInterface } from './../../models/users.interface';
+import { createAction, props } from '@ngrx/store';
+
 export const enum usersTypeActions {
   LOAD_USERS = '[LOAD_USERS] LOAD Users',
   LOAD_USERS_SUCCESS = '[LOAD_USERS_SUCCESS] LOAD Users with success',
@@ -19,3 +22,80 @@ export const enum usersTypeActions {
   DELETE_USER_SUCCESS = '[DELETE_USER_SUCCESS] DELETE User with success',
   DELETE_USER_FAIL = '[DELETE_USER_FAIL] Dont DELETE User',
 }
+
+//LOAD USERS //////////////////////////////////////
+export const LoadUsers = createAction(usersTypeActions.LOAD_USERS);
+
+export const LoadUsersSuccess = createAction(
+  usersTypeActions.LOAD_USERS_SUCCESS,
+  props<{ payload: UserInterface[] }>()
+);
+
+export const LoadUsersFail = createAction(
+  usersTypeActions.LOAD_USERS_FAIL,
+  props<{ error: string }>()
+);
+
+//LOAD USER //////////////////////////////////////
+export const LoadUser = createAction(
+  usersTypeActions.LOAD_USER,
+  props<{ payload: number }>()
+);
+
+export const LoadUserSuccess = createAction(
+  usersTypeActions.LOAD_USER_SUCCESS,
+  props<{ payload: UserInterface }>()
+);
+
+export const LoadUserFail = createAction(
+  usersTypeActions.LOAD_USER_FAIL,
+  props<{ error: string }>()
+);
+
+//CREATE USER ///////////////////////////////////
+export const CreateUser = createAction(
+  usersTypeActions.CREATE_USER,
+  props<{ payload: UserInterface }>()
+);
+
+export const CreateUserSuccess = createAction(
+  usersTypeActions.CREATE_USER_SUCCESS,
+  props<{ payload: UserInterface }>()
+);
+
+export const CreateUserFail = createAction(
+  usersTypeActions.CREATE_USER_FAIL,
+  props<{ error: string }>()
+);
+
+//UPDATE USER ///////////////////////////////////
+export const UpdateUser = createAction(
+  usersTypeActions.UPDATE_USER,
+  props<{ payload: UserInterface }>()
+);
+
+export const UpdateUserSuccess = createAction(
+  usersTypeActions.UPDATE_USER_SUCCESS,
+  props<{ payload: UserInterface }>()
+);
+
+export const UpdateUserFail = createAction(
+  usersTypeActions.UPDATE_USER_FAIL,
+  props<{ error: string }>()
+);
+
+//DELETE USER ///////////////////////////////////
+export const DeleteteUser = createAction(
+  usersTypeActions.DELETE_USER,
+  props<{ payload: number }>()
+);
+
+export const DeleteUserSuccess = createAction(
+  usersTypeActions.DELETE_USER_SUCCESS,
+  props<{ payload: number }>()
+);
+
+export const DeleteUserFail = createAction(
+  usersTypeActions.DELETE_USER_FAIL,
+  props<{ error: string }>()
+);
